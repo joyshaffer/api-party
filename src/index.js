fa432d304e3be848ad98dad0c19a20b534d94007
Joy Shaffer <jeshaffer@anderson.edu>
2017-07-11 14:45:51 -0400

    styled header

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BroweserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
     <Router>
